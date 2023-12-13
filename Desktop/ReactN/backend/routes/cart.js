0ef74bca99048fd885a764a79df9a68c29ed2cd3
Desmo-nd <mwrgdesmond@gmail.com>
2023-12-13 17:34:43 +0300

    Cart backend

---
+++ Desktop/ReactN/backend/routes/cart.js
@@ -0,0 +1,14 @@
+const router =require('express').Router();
+const cartController = require('../controllers/cartController');
+
+
+router.get("/find/:id", cartController.getcart);
+router.post("/", cartController.addTocart);
+router.post("/quantity", cartController.decrementCartItem);
+router.delete("/:cartItemId", cartController.deleteCartItem)
+
+
+
+
+
+module.exports = router;
