e2379a60556431c1925353382b7f8818e8e83184
simon-soen <in160005620@kisiiuniversity.ac.ke>
2023-12-15 12:00:37 +0300

    configured dependencies

---
+++ Desktop/ReactN/FrontEnd/Tom/screens/admin/DashboardScreen.jsx
@@ -0,0 +1,13 @@
+import { View, Text, StatusBar,  } from 'react-native'
+import React from 'react'
+
+const DashboardScreen = ({navigation, route}) => {
+  return (
+    <View style={styles.container}>
+      <StatusBar />
+      <View></View>
+    </View>
+  )
+}
+
+export default DashboardScreen
